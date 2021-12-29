bc2ba4c99e9f3029d3e87e4c77d469cf54b12986
Emanuele <emanuele.spreca@nttdata.com>
2021-12-29 12:05:35 +0100

    ytu

--- src/components/riepilogo/riepilogo.js
+++ src/components/riepilogo/riepilogo.js
@@ -10,6 +10,7 @@ class Riepilogo extends Component {
     super(props);
     this.state = {
       pesci: [],
+      pesciPezzature: [],
       expandedRows: null
     };
     this.rowExpansionTemplate = this.rowExpansionTemplate.bind(this);
@@ -22,11 +23,27 @@ class Riepilogo extends Component {
       .then((jsonObj) => {
         let obj = JSON.parse(jsonObj);
         let pesciList = [];
+        let pesciPezzatureList = [];
 
         for (let i in obj.pesci) {
-          if ((obj.pesci[i].isPeso && obj.pesci[i].peso > 0) ||
+          /*if ((obj.pesci[i].isPeso && obj.pesci[i].peso > 0) ||
             (obj.pesci[i].isPezzi && obj.pesci[i].pezzi > 0) ||
             (obj.pesci[i].isPezzatura && obj.pesci[i].pezzature.length !== 0)) {
+            obj.pesci[i].nome = obj.pesci[i].nome.replace(/([a-z0-9])([A-Z])/g, "$1 $2");
+            if (obj.pesci[i].peso > 0)
+              obj.pesci[i].peso = obj.pesci[i].peso.toFixed(3).replace('.', ',') + " kg";
+            pesciList.push(obj.pesci[i]);
+          }*/
+          if (obj.pesci[i].isPezzatura && obj.pesci[i].pezzature.length !== 0) {
+            obj.pesci[i].nome = obj.pesci[i].nome.replace(/([a-z0-9])([A-Z])/g, "$1 $2");
+            if (obj.pesci[i].peso > 0)
+              obj.pesci[i].peso = obj.pesci[i].peso.toFixed(3).replace('.', ',') + " kg";
+            for(let j in obj.pesci[i].pezzature){
+              obj.pesci[i].pezzature[j].peso = obj.pesci[i].pezzature[j].peso.toFixed(3).replace('.', ',') + " kg";
+            }
+            pesciPezzatureList.push(obj.pesci[i]);
+          } else if ((obj.pesci[i].isPeso && obj.pesci[i].peso > 0) ||
+            (obj.pesci[i].isPezzi && obj.pesci[i].pezzi > 0)) {
             obj.pesci[i].nome = obj.pesci[i].nome.replace(/([a-z0-9])([A-Z])/g, "$1 $2");
             if (obj.pesci[i].peso > 0)
               obj.pesci[i].peso = obj.pesci[i].peso.toFixed(3).replace('.', ',') + " kg";
@@ -36,6 +53,7 @@ class Riepilogo extends Component {
 
         this.setState({
           pesci: pesciList,
+          pesciPezzature: pesciPezzatureList,
         });
       });
   }
@@ -48,7 +66,19 @@ class Riepilogo extends Component {
         <div id="printSelector">
           <div className="dayR">{this.props.match.params.day}</div>
           <div id="toPrint" className="centerRiep">
-            <DataTable value={this.state.pesci} expandedRows={this.state.expandedRows} className="doubleRiepStyle"
+          <DataTable
+            sortMode="single"
+            sortField="peso"
+            sortOrder={-1}
+            value={this.state.pesci}
+            className="doubleRiepStyle centerTable"
+            emptyMessage="Nessun elemento presente"
+          >
+            <Column field="nome" header="Nome"></Column>
+            <Column field="peso" header="Quantità"></Column>
+            <Column field="pezzi" header="N. Pezzi"></Column>
+          </DataTable>
+            <DataTable value={this.state.pesciPezzature} expandedRows={this.state.expandedRows} className="doubleRiepStyle centerTable"
               onRowToggle={(e) => {
                 this.setState({
                   expandedRows: e.data
@@ -58,8 +88,8 @@ class Riepilogo extends Component {
             >
               <Column className="makeInvAfter" expander style={{ width: '3em' }} />
               <Column field="nome" header="Nome" />
+              <Column field="peso" header="Quantità" />
               <Column field="pezzi" header="N. Pezzi" />
-              <Column field="peso" header="Peso" />
             </DataTable>
           </div>
         </div>
@@ -70,10 +100,10 @@ class Riepilogo extends Component {
   rowExpansionTemplate(data) {
     return (
       <div>
-        <DataTable value={data.pezzature} className="doubleRiepStyle"
-        emptyMessage="Nessun elemento presente">
-          <Column field="n" header="N."></Column>
-          <Column field="peso" header="Peso"></Column>
+        <DataTable value={data.pezzature} className="expandedTable"
+          emptyMessage="Nessun elemento presente">
+          <Column field="n"></Column>
+          <Column field="peso"></Column>
         </DataTable>
       </div>
     );
